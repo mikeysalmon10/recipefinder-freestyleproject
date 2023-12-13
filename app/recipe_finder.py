5e13b2f074e753c2863adb3817324e2c7e1a6fbd
mikeysalmon10 <ms4697@georgetown.edu>
2023-12-13 17:02:33 -0500

    Added nutrition facts to webpage

--- app/recipe_finder.py
+++ app/recipe_finder.py
@@ -4,18 +4,60 @@ import requests
 from app.edamam import API_KEY, APP_ID
 
 def fetch_recipes(ingredients):
-    # Make sure to request all the necessary fields from the API
-    request_url = f"https://api.edamam.com/api/recipes/v2?type=public&q={'%20'.join(ingredients)}&app_id={APP_ID}&app_key={API_KEY}&field=label&field=ingredientLines&field=image&field=url"
+    request_url = f"https://api.edamam.com/api/recipes/v2?type=public&q={'%20'.join(ingredients)}&app_id={APP_ID}&app_key={API_KEY}&field=label&field=ingredientLines&field=image&field=url&field=totalNutrients&field=cuisineType&field=mealType"
     response = requests.get(request_url)
     if response.status_code == 200:
         return response.json()
     else:
         return None
+
         
 def display_recipe(recipe):
     print("Recipe:", recipe['label'])
-    print("Calories:", recipe['calories'])
+
     print("Ingredients:", ', '.join(recipe['ingredientLines']))
+
+    if 'cuisineType' in recipe:
+        print("Cuisine Type:", ', '.join(recipe['cuisineType']))
+    else:
+        print("Cuisine Type: Not available")
+
+    if 'mealType' in recipe:
+        print("Meal Type:", ', '.join(recipe['mealType']))
+    else:
+        print("Meal Type: Not available")
+
+    if 'totalNutrients' in recipe:
+        print("Nutritional Information:")
+        nutrients = recipe['totalNutrients']
+
+        # Extracting energy (calories), fat, carbohydrates, and protein
+        energy = nutrients.get('ENERC_KCAL', None)
+        fat = nutrients.get('FAT', None)
+        carbs = nutrients.get('CHOCDF', None)
+        protein = nutrients.get('PROCNT', None)
+
+        # Displaying the energy as calories
+        if energy:
+            print(f"Calories: {energy['quantity']} {energy['unit']}")
+        else:
+            print("Calories: Not available")
+
+        # Displaying other macronutrients
+        for nutrient, value in {'Fat': fat, 'Carbohydrates': carbs, 'Protein': protein}.items():
+            if value:
+                print(f"{nutrient}: {value['quantity']} {value['unit']}")
+            else:
+                print(f"{nutrient}: Not available")
+    else:
+        print("Nutritional Information: Not available")
+
+
+def display_instructions(recipe):
+    if 'instructions' in recipe:
+        print("Instructions:", recipe['instructions'])
+    else:
+        print("No instructions available for this recipe.")
 
 if __name__ == "__main__":
     print("RECIPE FINDER...")
@@ -30,5 +72,6 @@ if __name__ == "__main__":
         for i, item in enumerate(recipes_data["hits"], start=1):
             print("\nRecipe", i)
             display_recipe(item["recipe"])
+            display_instructions(item["recipe"])
     else:
         print("No recipes found.")
